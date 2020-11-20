0e4109d10e6b3f33145949c4665c244c909b7c2a
youugotssponged <jordanmccann64@outlook.com>
2020-11-20 22:20:59 +0000

    Initial Commit :)

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+
+namespace SpongeQR
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_GenerateQR_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
