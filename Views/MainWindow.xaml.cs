ab8129f3e4c1d4335bcca7917afca3d75005853f
youugotssponged <jordanmccann64@outlook.com>
2020-11-22 23:14:26 +0000

    Feat: Simple Message View, components added + test code

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,11 +1,13 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows;
-using QRCoder;
-
 using System.Windows.Media.Imaging;
+
 using Microsoft.Win32;
-using System;
-using static QRCoder.PayloadGenerator;
+
+using QRCoder;
+//using static QRCoder.PayloadGenerator;
+
 
 namespace SpongeQR
 {
@@ -24,19 +26,19 @@ namespace SpongeQR
             // TODO: ALLOW URL, AND OTHER THINGS TO BE ASSIGNED ALONG WITH DIRECTORY CHOICE AND FILE NAMING
 
             // CatJam URL Payload Test
-            Url generator = new Url("https://tenor.com/view/cat-jam-cat-jam-head-bounce-kitty-gif-17946989");
-            string payload = generator.ToString();
+            //Url generator = new Url("https://tenor.com/view/cat-jam-cat-jam-head-bounce-kitty-gif-17946989");
+            //string payload = generator.ToString();
 
             // Generate
             QRCodeGenerator qrGenerator = new QRCodeGenerator();
 
             // string param is the encoded link/message/thing
-            QRCodeData qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(TextBox_Message.Text, QRCodeGenerator.ECCLevel.Q);
             QRCode qrCode = new QRCode(qrCodeData);
             Bitmap qrCodeImage = qrCode.GetGraphic(20);
 
             // Save to same folder exe is in
-            qrCodeImage.Save("TEST.png"); // Save the bitmap image
+            qrCodeImage.Save($"{TextBox_QRImageName.Text}"+".png"); // Save the bitmap image
         }
 
         // Open a QR Code Image
@@ -52,5 +54,63 @@ namespace SpongeQR
                 image_viewer.Source = new BitmapImage(new Uri(op.FileName));
             }
         }
+
+        private void About_WindowMenuContext_Click(object sender, RoutedEventArgs e)
+        {
+            DevInfo devInfo = new DevInfo(
+                "1.0.1", 
+                "jordanmccann64@outlook.com",
+                "Thank you for using Sponge QR! \n\nHope it has helped you on your venture <3"
+            );
+
+            MessageBox.Show(devInfo.ToString(), "Sponge QR - About");
+        }
+
+        // Exit Handler
+        private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+        {
+            // Close Application
+            Application.Current.Shutdown();
+        }
+        private void MenuItemOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                image_viewer.Source = new BitmapImage(new Uri(op.FileName));
+            }
+        }
+
+        // Save Handler
+        private void btn_SaveQR(object sender, RoutedEventArgs e)
+        {
+
+            // Show Message Box upon save success
+        }
+
+        // Fires Upon Dropdown option Being Changed
+        private void EncodeChoiceDropDown_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            /*if (EncodeChoiceDropDown.SelectedIndex == (int)Options.A)
+            {
+                MessageBox.Show("MESSAGE");
+            } else if(EncodeChoiceDropDown.SelectedIndex == (int)Options.B)
+            {
+                MessageBox.Show("Email");
+            }*/
+        }
+
+
+        // Test representation of the dropdown selections
+        /*enum Options
+        {
+            A = 0,
+            B,
+            C
+        }*/
     }
 }
---
+++ src/DevInfo.cs
@@ -0,0 +1,25 @@
+﻿namespace SpongeQR
+{
+    class DevInfo
+    {
+        private string Version { get; set; }
+        private string Contact { get; set; }
+        private string Message { get; set; }
+
+        public DevInfo(string Version, string Contact, string Message)
+        {
+            this.Version = Version;
+            this.Contact = Contact;
+            this.Message = Message;
+        }
+
+        public override string ToString()
+        {
+            return $"SpongeQR was Developed By Jordan McCann (@youugotssponged).\n\n" +
+                $"Current Version: ({Version})\n\n" +
+                $"Contact: {Contact}\n\n" +
+                $"{Message}";
+        }
+
+    }
+}
